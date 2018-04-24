71cb4513fd2cf75cea95474a04c46d8a9695966a
BondarenkoDmitry <demianbondarenko@gmail.com>
2018-04-24 20:32:58 +0300

    Added onClickPremissionGranted to the UI

--- app/src/main/java/com/example/android/shushme/MainActivity.java
+++ app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -17,10 +17,12 @@ package com.example.android.shushme;
 */
 
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.LoaderManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -38,8 +40,10 @@ import com.google.android.gms.location.places.Places;
 
 
 public class MainActivity extends AppCompatActivity implements
-        ConnectionCallbacks,
-        OnConnectionFailedListener {
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener
+//        LoaderManager.LoaderCallbacks<Cursor>
+                                                 {
 
     // Constants
     public static final String TAG = MainActivity.class.getSimpleName();
